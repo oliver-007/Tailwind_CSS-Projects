061c52d5adc44750d2303400829cc67b8727e946
Oliver <oliverahmed000@gmail.com>
2022-01-30 21:31:20 +0600

    1st commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  darkMode: "class",
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ["Open Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
